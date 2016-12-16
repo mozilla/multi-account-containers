e286c2a3d44074fad84bb7f19ec72f5314fcbb9f
groovecoder <luke.crouch@gmail.com>
2016-12-16 11:02:57 -0600

    get identity name via regex from l10nID

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -1,7 +1,11 @@
+const IDENTITY_L10NID_MATCH_INDEX = 1;
+
 browser.runtime.sendMessage('get-identities').then(reply=> {
   if (reply) {
     reply.content.identities.forEach(identity=> {
-      document.querySelector('.identities-list').innerHTML += `<li><a href="#">${identity.icon}</a></li>`;
+      const identityName = identity.l10nID.match(/userContext(\w*)\.label/)[IDENTITY_L10NID_MATCH_INDEX];
+
+      document.querySelector('.identities-list').innerHTML += `<li><a href="#">${identityName}</a></li>`;
     });
     console.log('response from sdk addon: ', reply.content);
   }
