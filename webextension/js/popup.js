93fa66907170bfc906414fb8a9429d5408cf8d55
groovecoder <luke.crouch@gmail.com>
2016-12-20 11:05:37 -0600

    for #5: start browser action UI

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -4,8 +4,14 @@ browser.runtime.sendMessage('get-identities').then(reply=> {
   if (reply) {
     reply.content.identities.forEach(identity=> {
       const identityName = identity.l10nID.match(/userContext(\w*)\.label/)[IDENTITY_L10NID_MATCH_INDEX];
+      const identityRow = `
+      <tr>
+        <td>i</td>
+        <td>${identityName}</td>
+        <td>&gt;</td>
+      </tr>`;
 
-      document.querySelector('.identities-list').innerHTML += `<li><a href="#">${identityName}</a></li>`;
+      document.querySelector('.identities-list').innerHTML += identityRow;
     });
     console.log('response from sdk addon: ', reply.content);
   }
