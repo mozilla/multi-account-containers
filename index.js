17e16cab439b11f331b2ecde643e7d8f65478f53
Jonathan Kingston <jonathan@jooped.co.uk>
2016-12-20 21:03:28 +0000

    Link up experiment addon code to extension

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const webExtension = require('sdk/webextension');
 const {ContextualIdentityService} = require('resource://gre/modules/ContextualIdentityService.jsm');
 
 function handleWebExtensionMessage(message, sender, sendReply) {
-  console.log(message);
-  if (message === 'get-identities') {
-    sendReply({
-      content: {identities: ContextualIdentityService.getIdentities()}
-    });
-  }
 }
 
 webExtension.startup().then(api=> {
