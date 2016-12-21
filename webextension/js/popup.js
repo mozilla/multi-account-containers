5c738a7adc52578732a5f4f4a428ec901e29bab3
Jonathan Kingston <jonathan@jooped.co.uk>
2016-12-21 15:55:57 +0000

    Adding in container icons to menu

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -1,5 +1,11 @@
 browser.contextualIdentities.query({}).then((identites) => {
   identites.forEach((identity) => {
-    document.querySelector('.identities-list').innerHTML += `<li><a href="#">${identity.icon} ${identity.name}</a></li>`;
+    document.querySelector('.identities-list').innerHTML += `<li><a href="#">
+      <div
+        class="userContext-indicator"
+        data-identity-icon="${identity.icon}"
+        data-identity-color="${identity.color}"></div>
+      ${identity.name}
+    </a></li>`;
   });
 });
