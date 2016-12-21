4765a5f73a6aa0eab9a46348b24cde2693b93989
Jonathan Kingston <jonathan@jooped.co.uk>
2016-12-21 21:42:25 +0000

    Adding in hardcoded colours to match native test pilot

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -5,29 +5,16 @@ browser.contextualIdentities.query({}).then(identites=> {
   identites.forEach(identity=> {
     const identityRow = `
     <tr>
-      <td><div class="userContext-icon" data-identity-name="${identity.name}"></div></td>
+      <td><div class="userContext-icon"
+        data-identity-icon="${identity.icon}"
+        data-identity-color="${identity.color}"
+      ></div></td>
       <td>${identity.name}</td>
       <td>&gt;</td>
     </tr>`;
 
-    const customContainerStyle = `
-    [data-identity-name="${identity.name}"] {
-      --identity-icon: url('/img/usercontext.svg#${identity.icon}');
-      --identity-icon-color: ${identity.color};
-    }`;
-
-    customContainerStyles += customContainerStyle;
     identitiesListElement.innerHTML += identityRow;
   });
-
-  const customContainerStyleElement = document.createElement('style');
-
-  customContainerStyleElement.type = 'text/css';
-  customContainerStyleElement.appendChild(document.createTextNode(customContainerStyles));
-
-  const head = document.head;
-
-  head.appendChild(customContainerStyleElement);
 });
 
 document.querySelector('#edit-containers-link').addEventListener('click', ()=> {
