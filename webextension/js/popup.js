faf8a57854c4cc37dc1469d4aa2e39fb993e35ba
baku <baku@mozilla.com>
2017-01-06 20:12:46 +0100

    Fixing open about:preferences#containers

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -110,7 +110,10 @@ browser.runtime.sendMessage({method: 'queryIdentities'}).then(identities=> {
 
 
 document.querySelector('#edit-containers-link').addEventListener('click', ()=> {
-  browser.runtime.sendMessage('openContainersPreferences').then(()=> {
+  browser.runtime.sendMessage({
+    method: 'openTab',
+    url: "about:preferences#containers"
+  }).then(()=> {
     window.close();
   });
 });
