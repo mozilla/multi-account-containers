818658cc9c1505f635cc3166ed1fad837b1d29a4
Peter deHaan <peter@deseloper.com>
2017-01-16 14:25:25 -0800

    Add ESLint promise plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,26 @@ module.exports = {
   "extends": [
     "eslint:recommended"
   ],
+  "plugins": [
+    "promise"
+  ],
   "root": true,
   "rules": {
+    "promise/always-return": "off",
+    "promise/avoid-new": "off",
+    "promise/catch-or-return": "error",
+    "promise/no-callback-in-promise": "warn",
+    "promise/no-native": "off",
+    "promise/no-nesting": "warn",
+    "promise/no-promise-in-callback": "warn",
+    "promise/no-return-wrap": "error",
+    "promise/param-names": "error",
+
     "eqeqeq": "error",
     "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
     "no-throw-literal": "error",
+    "no-warning-comments": "warn",
     "quotes": ["error", "double"],
     "radix": "error",
     "semi": ["error", "always"]
