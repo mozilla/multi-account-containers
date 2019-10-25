8ae4a795c4dab7e3b5888ace6b085a9556375a23
Aishat Akinyemi <aishatadenike01@gmail.com>
2019-10-25 11:24:24 +0100

    refactor messageHandler.js to reuse badge.displayBrowserActionBadge

--- src/js/background/badge.js
+++ src/js/background/badge.js
@@ -1,20 +1,38 @@
 const MAJOR_VERSIONS = ["2.3.0", "2.4.0"];
 const badge = {
   async init() {
-    const currentWindow = await browser.windows.getCurrent();
-    this.displayBrowserActionBadge(currentWindow);
+    const showVersionIndicator = await browser.windows.getCurrent();
+    this.displayBrowserActionBadge(showVersionIndicator);
   },
 
-  async displayBrowserActionBadge() {
-    const extensionInfo = await backgroundLogic.getExtensionInfo();
-    const storage = await browser.storage.local.get({ browserActionBadgesClicked: [] });
+  disableAddon(tabId) {
+    browser.browserAction.disable(tabId);
+    browser.browserAction.setTitle({ tabId, title: "Containers disabled in Private Browsing Mode" });
+  },
 
-    if (MAJOR_VERSIONS.indexOf(extensionInfo.version) > -1 &&
-      storage.browserActionBadgesClicked.indexOf(extensionInfo.version) < 0) {
-      browser.browserAction.setBadgeBackgroundColor({ color: "rgba(0,217,0,255)" });
-      browser.browserAction.setBadgeText({ text: "NEW" });
+  async displayBrowserActionBadge(action) { 
+    const extensionInfo = await backgroundLogic.getExtensionInfo();
+    function changeBadgeColorText(color, text){
+      browser.browserAction.setBadgeBackgroundColor({color: color});
+      browser.browserAction.setBadgeText({text: text});
+    }
+    if(action==="showVersionIndicator") {    
+      const ActionBadgesClickedStorage = await browser.storage.local.get({browserActionBadgesClicked: []});
+      if (MAJOR_VERSIONS.indexOf(extensionInfo.version) > -1 &&
+          ActionBadgesClickedStorage.browserActionBadgesClicked.indexOf(extensionInfo.version) < 0) {
+        changeBadgeColorText("rgba(0,217,0,255)", "NEW");
+      }
+    }
+    else if (action==="showAchievement") {
+      const achievementsStorage = await browser.storage.local.get({achievements: []});
+      achievementsStorage.achievements.push({"name": "manyContainersOpened", "done": false});
+      // use set and spread to create a unique array
+      const achievements = [...new Set(achievementsStorage.achievements)];
+      browser.storage.local.set({achievements});
+      changeBadgeColorText("rgba(0,217,0,255)", "NEW");
     }
   }
+    
 };
 
 badge.init();
