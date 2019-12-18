62d1ea5b8a7282200ae54d580b9ffa2a9bf018a6
stoically <stoically@protonmail.com>
2019-12-18 09:18:10 +0100

    Replace manual mock/jsdom with webextensions-jsdom

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,10 +3,14 @@ module.exports = {
       "node": true,
       "mocha": true
     },
+    "parserOptions": {
+      "ecmaVersion": 2018
+    },
     globals: {
       "sinon": false,
       "expect": false,
       "nextTick": false,
+      "buildDom": false,
       "buildBackgroundDom": false,
       "background": false,
       "buildPopupDom": false,
