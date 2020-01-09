05b9e81b928a33c08ee6477d524dbf44d46c2f07
Kendall Werts <kmwerts@gmail.com>
2020-01-09 16:36:26 -0600

    added some checks on sync and some error handling for bad site assignment info

--- src/js/background/test.js
+++ src/js/background/test.js
@@ -1,16 +1,32 @@
 browser.tests = {
   async runAll() {
     await this.testIdentityStateCleanup();
-    await this.test1();
+    await this.testReconcileSiteAssignments();
+    await this.testInitialSync();
     await this.test2();
     await this.dupeTest();
+  },
+  // more unit tests
+  // if site assignment has no valid cookieStoreId, delete on local
+
+  async resetForManualTests() {
+    await browser.tests.stopSyncListeners();
+    console.log("reset");
+    await this.setState({}, LOCAL_DATA, TEST_CONTAINERS, []);
   },
 
   async testIdentityStateCleanup() {
     await browser.tests.stopSyncListeners();
     console.log("Testing the cleanup of local storage");
 
-    await this.setState({}, LOCAL_DATA, TEST_CONTAINERS);
+    await this.setState({}, LOCAL_DATA, TEST_CONTAINERS, []);
+
+    await browser.storage.local.set({
+      "identitiesState@@_firefox-container-5": { 
+        "hiddenTabs": [] 
+      }
+    });
+    console.log("local storage set: ", await browser.storage.local.get());
 
     await identityState.storageArea.cleanup();
 
@@ -28,11 +44,76 @@ browser.tests = {
     }
     console.log("Finished!");
   },
-  async test1() {
+
+  async testAssignManagerCleanup() {
+    await browser.tests.stopSyncListeners();
+    console.log("Testing the cleanup of local storage");
+
+    await this.setState({}, LOCAL_DATA, TEST_CONTAINERS, []);
+
+    await browser.storage.local.set({
+      "siteContainerMap@@_www.goop.com": { 
+        "userContextId": "5",
+        "neverAsk": true,
+        "hostname": "www.goop.com"
+      }
+    });
+    console.log("local storage set: ", await browser.storage.local.get());
+
+    await assignManager.storageArea.cleanup();
+
+    await browser.storage.local.set({
+      "identitiesState@@_firefox-container-5": { 
+        "hiddenTabs": [] 
+      }
+    });
+
+    const macConfigs = await browser.storage.local.get();
+    const assignments = [];
+    for(const configKey of Object.keys(macConfigs)) {
+      if (configKey.includes("siteContainerMap@@_")) {
+        assignments.push(configKey);
+      }
+    }
+
+    console.assert(assignments.length === 0, "There should be 0 identity entries");
+
+    console.log("Finished!");
+  },
+
+  async testReconcileSiteAssignments() {
+    await browser.tests.stopSyncListeners();
+    console.log("Testing reconciling Site Assignments");
+
+    const newSyncData = DUPE_TEST_SYNC;
+    // add some bad data.
+    newSyncData.assignedSites["siteContainerMap@@_www.linkedin.com"] = {
+      "userContextId": "1",
+      "neverAsk": true,
+      "hostname": "www.linkedin.com"
+    };
+
+    newSyncData.deletedSiteList = ["siteContainerMap@@_www.google.com"];
+
+    await this.setState(
+      newSyncData, 
+      LOCAL_DATA, 
+      TEST_CONTAINERS, 
+      SITE_ASSIGNMENT_TEST
+    );
+
+    await sync.runSync();
+
+    const assignedSites = await assignManager.storageArea.getAssignedSites();
+    console.assert(Object.keys(assignedSites).length === 5, "There should be 5 assigned sites");
+    console.log("Finished!");
+  },
+
+  async testInitialSync() {
     await browser.tests.stopSyncListeners();
     console.log("Testing new install with no sync");
 
-    await this.setState({}, LOCAL_DATA, TEST_CONTAINERS);
+    await this.setState({}, LOCAL_DATA, TEST_CONTAINERS, []);
 
     await sync.runSync();
 
@@ -106,7 +187,7 @@ browser.tests = {
 
   async dupeTest() {
     await browser.tests.stopSyncListeners();
-    console.log("Test state from duped sync");
+    console.log("Test state from sync that duped everything initially");
 
     await this.setState(
       DUPE_TEST_SYNC, 
@@ -176,30 +257,42 @@ browser.tests = {
     }
     return false;
   },
-  async setState(syncData, localData, indentityData, assignmentData){
+
+  /*
+   * Sets the state of sync storage, local storage, and the identities.
+   * SyncDat and localData (without identities or site assignments) get
+   * set to sync and local storage respectively. IdentityData creates
+   * new identities (after all have been removed), and assignmentData
+   * is used along with the new identities' cookieStoreIds to create
+   * site assignments in local storage.
+   */
+  async setState(syncData, localData, identityData, assignmentData){
     await this.removeAllContainers();
     await browser.storage.sync.clear();
     await browser.storage.sync.set(syncData);
     await browser.storage.local.clear();
-    for (let i=0; i < indentityData.length; i++) {
+    await browser.storage.local.set(localData);
+    for (let i=0; i < identityData.length; i++) {
       //build identities
       const newIdentity = 
-        await browser.contextualIdentities.create(indentityData[i]);
+        await browser.contextualIdentities.create(identityData[i]);
       // fill identies with site assignments
       if (assignmentData && assignmentData[i]) {
-        localData[assignmentData[i]] =  { 
+        const data =  { 
           "userContextId": 
             String(
               newIdentity.cookieStoreId.replace(/^firefox-container-/, "")
             ),
           "neverAsk": true
         };
+
+        await browser.storage.local.set({[assignmentData[i]]: data});
       }
     }
-    await browser.storage.local.set(localData);
     console.log("local storage set: ", await browser.storage.local.get());
     return;
   },
+
   async removeAllContainers() {
     const identities = await browser.contextualIdentities.query({});
     for (const identity of identities) {
@@ -254,6 +347,7 @@ const TEST_ASSIGNMENTS = [
   "siteContainerMap@@_www.linkedin.com",
   "siteContainerMap@@_reddit.com"
 ];
+
 const LOCAL_DATA = {
   "browserActionBadgesClicked": [ "6.1.1" ],
   "containerTabsOpened": 7,
@@ -312,23 +406,6 @@ const SYNC_DATA = {
     "firefox-container-150": "3dc916fb-8c0a-4538-9758-73ef819a45f7"
   },
   "assignedSites": {}
-};
-browser.resetMAC2 = async function () {
-  // for debugging and testing: remove all containers except the default 4 and the first one created
-  browser.tests.stopSyncListeners();
-
-  // sync state after FF1 (default + 1)
-  await browser.storage.sync.clear();
-  const syncData = {"cookieStoreIDmap":{"firefox-container-1":"4dc76734-5b71-4f2e-85d0-1cb199ae3821","firefox-container-2":"30308b8d-393c-4375-b9a1-afc59f0dea79","firefox-container-3":"7419c94d-85d7-4d76-94c0-bacef1de722f","firefox-container-4":"2b9fe881-e552-4df9-8cab-922f4688bb68","firefox-container-6":"db7f622e-682b-4556-968a-6e2542ff3b26"},"assignedSites":{"siteContainerMap@@_twitter.com":{"userContextId":"1","neverAsk":!0},"siteContainerMap@@_www.facebook.com":{"userContextId":"2","neverAsk":!0},"siteContainerMap@@_www.linkedin.com":{"userContextId":"4","neverAsk":!1}},"identities":[{"name":"Personal","icon":"fingerprint","iconUrl":"resource://usercontext-content/fingerprint.svg","color":"blue","colorCode":"#37adff","cookieStoreId":"firefox-container-1"},{"name":"Work","icon":"briefcase","iconUrl":"resource://usercontext-content/briefcase.svg","color":"orange","colorCode":"#ff9f00","cookieStoreId":"firefox-container-2"},{"name":"Banking","icon":"dollar","iconUrl":"resource://usercontext-content/dollar.svg","color":"green","colorCode":"#51cd00","cookieStoreId":"firefox-container-3"},{"name":"Shopping","icon":"cart","iconUrl":"resource://usercontext-content/cart.svg","color":"pink","colorCode":"#ff4bda","cookieStoreId":"firefox-container-4"},{"name":"Container #01","icon":"chill","iconUrl":"resource://usercontext-content/chill.svg","color":"green","colorCode":"#51cd00","cookieStoreId":"firefox-container-6"}]};
-  browser.storage.sync.set(syncData);
-
-  // FF2 (intial sync w/ default 4 + 1 with some changes)
-  browser.contextualIdentities.update("firefox-container-2", {color:"purple"});
-  browser.contextualIdentities.update("firefox-container-4", {icon:"pet"});
-  browser.storage.local.clear();
-  const localData = {"browserActionBadgesClicked":["6.1.1"],"containerTabsOpened":7,"identitiesState@@_firefox-container-1":{"hiddenTabs":[]},"identitiesState@@_firefox-container-2":{"hiddenTabs":[]},"identitiesState@@_firefox-container-3":{"hiddenTabs":[]},"identitiesState@@_firefox-container-4":{"hiddenTabs":[]},"identitiesState@@_firefox-container-6":{"hiddenTabs":[]},"identitiesState@@_firefox-default":{"hiddenTabs":[]},"onboarding-stage":5,"siteContainerMap@@_developer.mozilla.org":{"userContextId":"6","neverAsk":!1},"siteContainerMap@@_twitter.com":{"userContextId":"1","neverAsk":!0},"siteContainerMap@@_www.linkedin.com":{"userContextId":"4","neverAsk":!1}};
-  browser.storage.local.set(localData);
-
 };
 
 const DUPE_TEST_SYNC = {
@@ -439,6 +516,13 @@ const DUPE_TEST_ASSIGNMENTS = [
   "siteContainerMap@@_www.linkedin.com"
 ];
 
+const SITE_ASSIGNMENT_TEST = [
+  "siteContainerMap@@_developer.mozilla.org",
+  "siteContainerMap@@_www.facebook.com",
+  "siteContainerMap@@_www.google.com",
+  "siteContainerMap@@_bugzilla.mozilla.org"
+];
+
 const DUPE_TEST_IDENTS = [
   {
     "name": "Personal",
@@ -471,38 +555,3 @@ const DUPE_TEST_IDENTS = [
     "color": "yellow",
   }
 ];
-browser.resetMAC3 = async function () {
-  // for debugging and testing: remove all containers except the default 4 and the first one created
-  browser.tests.stopSyncListeners();
-
-  // sync state after FF2 synced
-  await browser.storage.sync.clear();
-  const syncData = {"assignedSites":{"siteContainerMap@@_developer.mozilla.org":{"userContextId":"6","neverAsk":!1,"hostname":"developer.mozilla.org"},"siteContainerMap@@_twitter.com":{"userContextId":"1","neverAsk":!0,"hostname":"twitter.com"},"siteContainerMap@@_www.facebook.com":{"userContextId":"2","neverAsk":!0,"hostname":"www.facebook.com"},"siteContainerMap@@_www.linkedin.com":{"userContextId":"4","neverAsk":!1,"hostname":"www.linkedin.com"},"siteContainerMap@@_reddit.com": {"userContextId": "7","neverAsk": true}},"cookieStoreIDmap":{"firefox-container-1":"4dc76734-5b71-4f2e-85d0-1cb199ae3821","firefox-container-2":"30308b8d-393c-4375-b9a1-afc59f0dea79","firefox-container-3":"7419c94d-85d7-4d76-94c0-bacef1de722f","firefox-container-4":"2b9fe881-e552-4df9-8cab-922f4688bb68","firefox-container-6":"db7f622e-682b-4556-968a-6e2542ff3b26","firefox-container-7":"ceb06672-76c0-48c4-959e-f3a3ee8358b6"},"identities":[{"name":"Personal","icon":"fingerprint","iconUrl":"resource://usercontext-content/fingerprint.svg","color":"blue","colorCode":"#37adff","cookieStoreId":"firefox-container-1"},{"name":"Work","icon":"briefcase","iconUrl":"resource://usercontext-content/briefcase.svg","color":"orange","colorCode":"#ff9f00","cookieStoreId":"firefox-container-2"},{"name":"Banking","icon":"dollar","iconUrl":"resource://usercontext-content/dollar.svg","color":"purple","colorCode":"#af51f5","cookieStoreId":"firefox-container-3"},{"name":"Shopping","icon":"cart","iconUrl":"resource://usercontext-content/cart.svg","color":"pink","colorCode":"#ff4bda","cookieStoreId":"firefox-container-4"},{"name":"Container #01","icon":"chill","iconUrl":"resource://usercontext-content/chill.svg","color":"green","colorCode":"#51cd00","cookieStoreId":"firefox-container-6"},{"name":"Container #02","icon":"vacation","iconUrl":"resource://usercontext-content/vacation.svg","color":"yellow","colorCode":"#ffcb00","cookieStoreId":"firefox-container-7"}]};
-  browser.storage.sync.set(syncData);
-
-  // FF1 with updates from FF2 (intial sync w/ default 4 + 1 with some changes)
-  browser.contextualIdentities.update("firefox-container-3", {color:"purple", icon:"fruit"});
-  //browser.contextualIdentities.create({name: "Container #02", icon: "vacation", color: "yellow"});
-  browser.storage.local.clear();
-  const localData = {"beenSynced":!0,"browserActionBadgesClicked":["6.1.1"],"containerTabsOpened":7,"identitiesState@@_firefox-container-1":{"hiddenTabs":[],"macAddonUUID":"4dc76734-5b71-4f2e-85d0-1cb199ae3821"},"identitiesState@@_firefox-container-2":{"hiddenTabs":[],"macAddonUUID":"30308b8d-393c-4375-b9a1-afc59f0dea79"},"identitiesState@@_firefox-container-3":{"hiddenTabs":[],"macAddonUUID":"7419c94d-85d7-4d76-94c0-bacef1de722f"},"identitiesState@@_firefox-container-4":{"hiddenTabs":[],"macAddonUUID":"2b9fe881-e552-4df9-8cab-922f4688bb68"},"identitiesState@@_firefox-container-6":{"hiddenTabs":[],"macAddonUUID":"db7f622e-682b-4556-968a-6e2542ff3b26"},"identitiesState@@_firefox-default":{"hiddenTabs":[]},"onboarding-stage":5,"siteContainerMap@@_developer.mozilla.org":{"userContextId":"6","neverAsk":!1},"siteContainerMap@@_twitter.com":{"userContextId":"1","neverAsk":!0},"siteContainerMap@@_www.facebook.com":{"userContextId":"2","neverAsk":!0},"siteContainerMap@@_www.linkedin.com":{"userContextId":"4","neverAsk":!1}};
-  browser.storage.local.set(localData);
-
-};
-
-browser.resetMAC4 = async function () {
-  // for debugging and testing: remove all containers except the default 4 and the first one created
-  browser.tests.stopSyncListeners();
-
-  // sync state after FF2 synced
-  await browser.storage.sync.clear();
-  const syncData = {"assignedSites":{"siteContainerMap@@_developer.mozilla.org":{"userContextId":"6","neverAsk":!1,"hostname":"developer.mozilla.org"},"siteContainerMap@@_twitter.com":{"userContextId":"1","neverAsk":!0,"hostname":"twitter.com"},"siteContainerMap@@_www.facebook.com":{"userContextId":"2","neverAsk":!0,"hostname":"www.facebook.com"},"siteContainerMap@@_www.linkedin.com":{"userContextId":"4","neverAsk":!1,"hostname":"www.linkedin.com"},"siteContainerMap@@_reddit.com": {"userContextId": "7","neverAsk": true}},"cookieStoreIDmap":{"firefox-container-1":"4dc76734-5b71-4f2e-85d0-1cb199ae3821","firefox-container-2":"30308b8d-393c-4375-b9a1-afc59f0dea79","firefox-container-3":"7419c94d-85d7-4d76-94c0-bacef1de722f","firefox-container-4":"2b9fe881-e552-4df9-8cab-922f4688bb68","firefox-container-6":"db7f622e-682b-4556-968a-6e2542ff3b26","firefox-container-7":"ceb06672-76c0-48c4-959e-f3a3ee8358b6"},"identities":[{"name":"Personal","icon":"fingerprint","iconUrl":"resource://usercontext-content/fingerprint.svg","color":"blue","colorCode":"#37adff","cookieStoreId":"firefox-container-1"},{"name":"Work","icon":"briefcase","iconUrl":"resource://usercontext-content/briefcase.svg","color":"orange","colorCode":"#ff9f00","cookieStoreId":"firefox-container-2"},{"name":"Banking","icon":"dollar","iconUrl":"resource://usercontext-content/dollar.svg","color":"purple","colorCode":"#af51f5","cookieStoreId":"firefox-container-3"},{"name":"Shopping","icon":"cart","iconUrl":"resource://usercontext-content/cart.svg","color":"pink","colorCode":"#ff4bda","cookieStoreId":"firefox-container-4"},{"name":"Container #01","icon":"chill","iconUrl":"resource://usercontext-content/chill.svg","color":"green","colorCode":"#51cd00","cookieStoreId":"firefox-container-6"},{"name":"Container #02","icon":"vacation","iconUrl":"resource://usercontext-content/vacation.svg","color":"yellow","colorCode":"#ffcb00","cookieStoreId":"firefox-container-7"}]};
-  browser.storage.sync.set(syncData);
-
-  // FF1 with updates from FF2 (intial sync w/ default 4 + 1 with some changes)
-  browser.contextualIdentities.update("firefox-container-3", {color:"purple", icon:"fruit"});
-  //browser.contextualIdentities.create({name: "Container #02", icon: "vacation", color: "yellow"});
-  browser.storage.local.clear();
-  const localData = {"beenSynced":!0,"browserActionBadgesClicked":["6.1.1"],"containerTabsOpened":7,"identitiesState@@_firefox-container-1":{"hiddenTabs":[],"macAddonUUID":"4dc76734-5b71-4f2e-85d0-1cb199ae3821"},"identitiesState@@_firefox-container-2":{"hiddenTabs":[],"macAddonUUID":"30308b8d-393c-4375-b9a1-afc59f0dea79"},"identitiesState@@_firefox-container-3":{"hiddenTabs":[],"macAddonUUID":"7419c94d-85d7-4d76-94c0-bacef1de722f"},"identitiesState@@_firefox-container-4":{"hiddenTabs":[],"macAddonUUID":"2b9fe881-e552-4df9-8cab-922f4688bb68"},"identitiesState@@_firefox-container-6":{"hiddenTabs":[],"macAddonUUID":"db7f622e-682b-4556-968a-6e2542ff3b26"},"identitiesState@@_firefox-default":{"hiddenTabs":[]},"onboarding-stage":5,"siteContainerMap@@_developer.mozilla.org":{"userContextId":"6","neverAsk":!1},"siteContainerMap@@_twitter.com":{"userContextId":"1","neverAsk":!0},"siteContainerMap@@_www.facebook.com":{"userContextId":"2","neverAsk":!0},"siteContainerMap@@_www.linkedin.com":{"userContextId":"4","neverAsk":!1}};
-  browser.storage.local.set(localData);
-
-};
