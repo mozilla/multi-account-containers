393d6228e012496fc088953d83ce076a5f8c55b6
Kendall Werts <kmwerts@gmail.com>
2020-02-20 14:39:54 -0600

    css linting

--- src/js/popup.js
+++ src/js/popup.js
@@ -715,10 +715,7 @@ Logic.registerPanel(P_CONTAINER_INFO, {
       window.close();
     });
 
-    const manageContainer = document.querySelector("#manage-container-link");
-    Utils.addEnterHandler(manageContainer, async () => {
-      Logic.showPanel(P_CONTAINER_EDIT, Logic.currentIdentity());
-    });
+
   },
 
   // This method is called when the panel is shown.
@@ -751,6 +748,10 @@ Logic.registerPanel(P_CONTAINER_INFO, {
       method: "getTabs",
       windowId: browser.windows.WINDOW_ID_CURRENT,
       cookieStoreId: Logic.currentIdentity().cookieStoreId
+    });
+    const manageContainer = document.querySelector("#manage-container-link");
+    Utils.addEnterHandler(manageContainer, async () => {
+      Logic.showPanel(P_CONTAINER_EDIT, identity);
     });
     return this.buildOpenTabTable(tabs);
   },
