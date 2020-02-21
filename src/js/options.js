6330eca2d3e9f8baa503e8c0320856818a9511e8
Kendall Werts <kmwerts@gmail.com>
2020-02-21 09:26:08 -0600

    keyboard shortcuts

--- src/js/options.js
+++ src/js/options.js
@@ -1,3 +1,5 @@
+const NUMBER_OF_KEYBOARD_SHORTCUTS = 2;
+
 async function requestPermissions() {
   const checkbox = document.querySelector("#bookmarksPermissions");
   if (checkbox.checked) {
@@ -22,7 +24,8 @@ async function enableDisableSync() {
   browser.runtime.sendMessage({ method: "resetSync" });
 }
 
-async function restoreOptions() {
+async function setupOptions() {
+  console.log("setup")
   const hasPermission = await browser.permissions.contains({permissions: ["bookmarks"]});
   const { syncEnabled } = await browser.storage.local.get("syncEnabled");
   if (hasPermission) {
@@ -33,9 +36,50 @@ async function restoreOptions() {
   } else {
     document.querySelector("#syncCheck").checked = false;
   }
+  setupContainerShortcutSelects();
 }
 
+async function setupContainerShortcutSelects () {
+  const keyboardShortcut = browser.runtime.sendMessage({method: "getShortcuts"});
+  console.log(keyboardShortcut);
+  const identities = await browser.contextualIdentities.query({});
+  const fragment = document.createDocumentFragment();
+  const noneOption = document.createElement("option");
+  noneOption.value = "none";
+  noneOption.textContent = "None";
+  fragment.append(noneOption);
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
+  for (const identity of identities) {
+    const option = document.createElement("option");
+    option.value = identity.cookieStoreId;
+    option.id = identity.cookieStoreId;
+    option.textContent = identity.name;
+    fragment.append(option);
+  }
+
+  for (let i=0; i < NUMBER_OF_KEYBOARD_SHORTCUTS; i++) {
+    const shortcutKey = "open_container_"+i;
+    const shortcutSelect = document.getElementById(shortcutKey);
+    shortcutSelect.appendChild(fragment.cloneNode(true));
+    if (keyboardShortcut && keyboardShortcut[shortcutKey]) {
+      shortcutSelect.getElementById(keyboardShortcut[shortcutKey]).selected = true;
+    }
+  }
+}
+
+function storeShortcutChoice (event) {
+  browser.runtime.sendMessage({
+    method: "setShortcut",
+    shortcut: event.target.id,
+    cookieStoreId: event.target.value
+  });
+}
+
+document.addEventListener("DOMContentLoaded", setupOptions);
 document.querySelector("#bookmarksPermissions").addEventListener( "change", requestPermissions);
 document.querySelector("#syncCheck").addEventListener( "change", enableDisableSync);
+
+for (let i=0; i < NUMBER_OF_KEYBOARD_SHORTCUTS; i++) {
+  document.querySelector("#open_container_"+i)
+    .addEventListener("change", storeShortcutChoice);
+}
