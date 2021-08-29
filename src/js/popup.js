c9b9a1dbcec427d43948141f538642f2fae8fd39
Martin D Kealey <martin@kurahaupo.gen.nz>
2021-08-30 00:49:18 +1000

    Bugfix for 74365a5d Logic.addEnterHandler -> Utils.addEnterHandler

--- src/js/popup.js
+++ src/js/popup.js
@@ -1821,7 +1821,7 @@ Logic.registerPanel(P_CONTAINER_EDIT, {
 
     // Add new site to current container
     const siteLink = document.querySelector("#edit-container-site-link");
-    Logic.addEnterHandler(siteLink, () => {
+    Utils.addEnterHandler(siteLink, () => {
       this._addSite();
     });
   },
@@ -1958,7 +1958,7 @@ Logic.registerPanel(P_CONTAINER_EDIT, {
         trElement.getElementsByClassName("favicon")[0].appendChild(Utils.createFavIconElement(assumedUrl));
         const deleteButton = trElement.querySelector(".delete-assignment");
         const that = this;
-        Logic.addEnterHandler(deleteButton, async () => {
+        Utils.addEnterHandler(deleteButton, async () => {
           const userContextId = Logic.currentUserContextId();
           // Lets show the message to the current tab
           // TODO remove then when firefox supports arrow fn async
